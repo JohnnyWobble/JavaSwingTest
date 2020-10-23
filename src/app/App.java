a405efc342742fd7af60a009469d009c37410323
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2020-10-23 17:57:20 -0400

    First

---
+++ src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+import app.UI;
+
+public class App {
+    public static void main(String[] args) {
+        UI a = new UI();
+    }
+}
---
+++ src/app/UI.java
@@ -0,0 +1,10 @@
+package app;
+
+import javafx.application.Application;
+
+public class UI extends Application {
+    
+    public UI() {
+    }
+}
+    
